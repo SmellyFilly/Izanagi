0eb50790ad3bb1a23a75c62bdf55a17f50a821b9
SmellyFilly <vipakilla@gmail.com>
2022-12-25 01:47:08 +1000

    Initial Commit

---
+++ Izanagi/IzanagiApp.swift
@@ -0,0 +1,17 @@
+//
+//  IzanagiApp.swift
+//  Izanagi
+//
+//  Created by Daniel Hummelstad on 25/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct IzanagiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
